a374e5e011fc869b3cf0ac953461628ce1cd85ae
varioustoxins <garyt.and.sarahb@gmail.com>
2024-07-01 16:42:56 +0200

    changes to fitter for NEF relaxation draft proposal implimentation [much simpler interface]

--- src/streamfitter/fitter.py
+++ src/streamfitter/fitter.py
@@ -1,20 +1,13 @@
 import multiprocessing
 import time
-from collections import Counter
 from dataclasses import dataclass
 from datetime import timedelta
 from enum import StrEnum, auto
 from itertools import combinations
-from typing import List, Dict
+from typing import Dict
 
-from nef_pipelines.lib.shift_lib import IntensityMeasurementType
-from nef_pipelines.lib.util import exit_error
 from numpy.random import normal
 
-
-from statistics import stdev
-
-from pynmrstar import Entry, Saveframe
 from jax.numpy import exp, where, log, array
 from numpy import random as numpy_random
 
@@ -24,6 +17,8 @@ from inspect import signature
 from jax import jacfwd
 from jax import __version__ as jax_version
 from numpy import __version__ as numpy_version
+
+from nef_pipelines.tools.fit.fit_lib import _get_noise_from_duplicated_values
 from streamfitter import __version__ as streamfitter_version
 
 from classprop import classprop
@@ -96,35 +91,36 @@ class RunningStats(object):
         return result
 
 
-def _calculate_monte_carlo_error(fitter, xs, fits, error_method, noise_level, num_cycles, validate_mc=False):
-    xs_as_floats = [float(x) for x in xs]
+def _calculate_monte_carlo_error(fitter, id_xy_data, fits, error_method, noise_level, num_cycles, validate_mc=False):
     if validate_mc:
-        replicate_xs = [value for value, count in Counter(xs_as_floats).items() if count > 1]
         replicate_averages = RunningStats()
 
     mc_fitted_params = {}
     mc_value_stats = {}
     mc_fitted_param_values = {}
-    for row_count, (atom_key, fit) in enumerate(fits.items()):
-        value_stats = {(i, value): RunningStats() for i, value in enumerate(xs_as_floats)}
-        mc_value_stats[atom_key] = value_stats
+    for row_count, (id, fit) in enumerate(fits.items()):
+        # jax objects are not hashable
+        xs = id_xy_data[id][0]
+        xs_as_floats = [float(elem) for elem in xs]
+        value_stats = {(id, x): RunningStats() for x in xs_as_floats}
+
+        mc_value_stats[id] = value_stats
         mc_fitted_param_list = []
-        mc_fitted_param_values[atom_key] = mc_fitted_param_list
+        mc_fitted_param_values[id] = mc_fitted_param_list
 
         fitted_params = [value.value for value in fit.params.values()]
 
-        back_calculated = fitter.function(*fitted_params, xs)
+        back_calculated = fitter.function(*fitted_params, id_xy_data[id][0])
 
         mc_keys_and_values = {}
         for i in range(num_cycles):
-            fit_key = atom_key, i
-            mc_data = back_calculated + normal(0, noise_level, len(xs))
+            fit_key = id, i
+            mc_data = back_calculated + normal(0, noise_level, len(xs_as_floats))
 
             if validate_mc:
                 replicate_values = {}
                 for point, data in zip(xs_as_floats, mc_data):
-                    if point in replicate_xs:
-                        replicate_values.setdefault(point, []).append(float(data))
+                    replicate_values.setdefault(point, []).append(float(data))
                 for replicate in replicate_values.values():
                     for combination in combinations(replicate, 2):
                         replicate_averages.add(combination[0] - combination[1])
@@ -150,7 +146,7 @@ def _calculate_monte_carlo_error(fitter, xs, fits, error_method, noise_level, nu
                     averager.add(back_calculated)
 
         errors = {f'{name}_mc_error': averager.sterr() for name, averager in averagers.items()}
-        mc_fitted_params[atom_key] = {
+        mc_fitted_params[id] = {
             **errors,
             '%mc_failures': (num_cycles - mc_calculations) / num_cycles * 100,
         }
@@ -190,111 +186,59 @@ class NoNEFPipeslinesError(Exception):
 
 
 def fitter(
-    entry: Entry,
-    series_frames: List[Saveframe],
+    id_xy_data,
     error_method: ErrorPropogation,
     cycles: int,
     noise_level,
-    intensity_measurement_type: IntensityMeasurementType,
     seed: int,
 ) -> Dict:
     _import_nef_pipelines_or_raise()
 
-    from nef_pipelines.lib.peak_lib import frame_to_peaks
+    id_xy_data = {id: (array(xs), array(ys)) for id, (xs, ys) in id_xy_data.items()}
 
     numpy_random.seed(seed)
 
     results = []
-    for series_frame in series_frames:
-        series_experiment_loop = get_loop(series_frame, 'nef_series_experiment')
 
-        _exit_if_no_series_lists_selected(series_frame, series_experiment_loop)
+    replicate_noise_level, stderr, num_replicates = _get_noise_from_duplicated_values(id_xy_data)
 
-        spectra_by_times_and_indices = _get_spectra_by_series_variable(entry, series_experiment_loop)
+    noise_source = 'cli' if noise_level else 'replicates'
+    noise_level = noise_level or replicate_noise_level
 
-        _exit_if_spectra_are_missing(spectra_by_times_and_indices, series_frame.name)
+    fitter = Relaxation2PointFitter()
 
-        peaks_by_times_and_indices = {
-            key: frame_to_peaks(spectrum_frame) for key, spectrum_frame in spectra_by_times_and_indices.items()
-        }
+    # TODO: this could be a context manager
+    start_time = time.time()
+    fits = _fit_series(id_xy_data, fitter)
 
-        atoms_and_values = _get_atoms_and_values(peaks_by_times_and_indices, intensity_measurement_type)
+    monte_carlo_errors, monte_carlo_value_stats, monte_carlo_param_values = _calculate_monte_carlo_error(
+        fitter, id_xy_data, fits, error_method, noise_level, cycles
+    )
 
-        replicate_noise_level, stderr, num_replicates = _get_noise_from_duplicated_values(
-            peaks_by_times_and_indices, intensity_measurement_type
-        )
+    end_time = time.time()
 
-        noise_source = 'cli' if noise_level else 'replicates'
-        noise_level = noise_level or replicate_noise_level
+    time_delta = timedelta(seconds=end_time - start_time)
 
-        fitter = Relaxation2PointFitter()
+    versions_string = (
+        f'stream_fitter [{streamfitter_version}], lmfit [{lmfit_version}], jax[{jax_version}], numpy[{numpy_version}'
+    )
 
-        # TODO: this could be a context manager
-        start_time = time.time()
-        fits = _fit_series(atoms_and_values, fitter)
-
-        xs = _get_series_variables_array(series_experiment_loop)
-
-        monte_carlo_errors, monte_carlo_value_stats, monte_carlo_param_values = _calculate_monte_carlo_error(
-            fitter, xs, fits, error_method, noise_level, cycles
-        )
-        end_time = time.time()
-
-        time_delta = timedelta(seconds=end_time - start_time)
-
-        versions_string = f'stream_fitter [{streamfitter_version}], lmfit [{lmfit_version}], jax[{jax_version}], numpy[{numpy_version}'
-
-        results.append(
-            {
-                'fits': fits,
-                'monte_carlo_errors': monte_carlo_errors,
-                'monte_carlo_value_stats': monte_carlo_value_stats,
-                'monte_carlo_param_values': monte_carlo_param_values,
-                'versions': versions_string,
-                'calculation_time': time_delta,
-                'number of cpus': multiprocessing.cpu_count(),
-                'random seed': seed,
-                'noise_level': noise_level,
-                'error in noise estimate': stderr,
-                'source of noise estimate': noise_source,
-                'number of replicates': num_replicates,
-            }
-        )
+    results = {
+        'fits': fits,
+        'monte_carlo_errors': monte_carlo_errors,
+        'monte_carlo_value_stats': monte_carlo_value_stats,
+        'monte_carlo_param_values': monte_carlo_param_values,
+        'versions': versions_string,
+        'calculation_time': time_delta,
+        'number of cpus': multiprocessing.cpu_count(),
+        'random seed': seed,
+        'noise_level': noise_level,
+        'error in noise estimate': stderr,
+        'source of noise estimate': noise_source,
+        'number of replicates': num_replicates,
+    }
 
     return results
-
-
-def _get_noise_from_duplicated_values(peaks_by_times_and_indices, data_type: IntensityMeasurementType) -> float:
-    peak_list_keys_by_times = {}
-    for key, peak_list in peaks_by_times_and_indices.items():
-        _, value, _ = key
-        peak_list_keys_by_times.setdefault(value, []).append(key)
-
-    duplicated_peak_list_keys = [
-        peak_list_keys for peak_list_keys in peak_list_keys_by_times.values() if len(peak_list_keys) > 1
-    ]
-
-    differences = []
-    for duplicated_peak_list_set in duplicated_peak_list_keys:
-        for combination in combinations(duplicated_peak_list_set, 2):
-            peak_pairs = {}
-            for key in combination:
-                for peak in peaks_by_times_and_indices[key]:
-                    if data_type == IntensityMeasurementType.HEIGHT:
-                        value = peak.height
-                    elif data_type == IntensityMeasurementType.VOLUME:
-                        value = peak.volume
-                    key = tuple(sorted([shift.atom for shift in peak.shifts]))
-                    peak_pairs.setdefault(key, []).append(value)
-
-            for value_pair in peak_pairs.values():
-                if len(value_pair) == 2:
-                    differences.append(value_pair[0] - value_pair[1])
-
-    replicates_stdev = stdev(differences)
-    replicates_stderr = replicates_stdev / len(differences) ** 0.5
-
-    return replicates_stdev, replicates_stderr / replicates_stdev, len(differences)
 
 
 def _import_nef_pipelines_or_raise():
@@ -304,20 +248,16 @@ def _import_nef_pipelines_or_raise():
         raise NoNEFPipeslinesError()
 
 
-def _get_series_variables_array(series_experiment_loop):
-    from nef_pipelines.lib.nef_lib import loop_row_namespace_iter
-
-    return array([row.series_variable for row in loop_row_namespace_iter(series_experiment_loop, convert=True)])
+def _get_series_variables_array(id_xy_data):
+    return array([xy_data[0] for xy_data in id_xy_data.values()])
 
 
-def _fit_series(atoms_and_values, fitter):
+def _fit_series(ids_and_values, fitter):
     func = FunctionWrapper(fitter.function)
     jacobian = JacobianWrapper(fitter.function)
 
     fits = {}
-    for atom_key, (xs, ys) in atoms_and_values.items():
-        xs = array(xs)
-        ys = array(ys)
+    for id, (xs, ys) in ids_and_values.items():
         params = Parameters()
         estimated_parameters_dict = fitter.estimate(xs, ys)
         for key, value in estimated_parameters_dict.items():
@@ -326,7 +266,7 @@ def _fit_series(atoms_and_values, fitter):
         minimizer = Minimizer(func, params, fcn_args=(xs,), fcn_kws={'data': ys})
         out = minimizer.leastsq(Dfun=jacobian, col_deriv=1)
 
-        fits[atom_key] = out
+        fits[id] = out
 
     return fits
 
@@ -391,59 +331,3 @@ class Relaxation2PointFitter:
     @classprop
     def params(cls):
         return list(signature(cls.function).parameters.keys())[:-1]
-
-
-def _get_atoms_and_values(peaks_by_times_and_indices, data_type: IntensityMeasurementType) -> Dict:
-    atoms_to_values = {}
-    for (index, x_value, spectrum_name), peaks in peaks_by_times_and_indices.items():
-        for peak in peaks:
-            atoms = tuple(sorted([shift.atom for shift in peak.shifts]))
-            x_and_y = atoms_to_values.setdefault(atoms, [[], []])
-
-            y_value = peak.height if data_type == IntensityMeasurementType.HEIGHT else peak.volume
-            x_and_y[0].append(x_value)
-            x_and_y[1].append(y_value)
-
-    return atoms_to_values
-
-
-def _get_spectra_by_series_variable(entry, series_experiment_loop):
-    from nef_pipelines.lib.nef_lib import loop_row_namespace_iter
-
-    spectra_by_times = {}
-    for i, row in enumerate(loop_row_namespace_iter(series_experiment_loop, convert=True), start=1):
-        spectrum_frame_name = row.nmr_spectrum_id
-        series_variable = row.series_variable
-
-        spectrum_frame = entry.get_saveframe_by_name(spectrum_frame_name)
-
-        key = i, series_variable, spectrum_frame_name
-        spectra_by_times[key] = spectrum_frame
-
-    return spectra_by_times
-
-
-def _exit_if_no_series_lists_selected(frame, series_experiment_loop):
-    if not series_experiment_loop:
-        msg = f'no nef series experiment loop found in frame {frame.name}'
-        exit_error(msg)
-
-
-# TODO: move to lib
-def get_loop(frame, loop_name):
-    try:
-        series_experiment_loop = frame.get_loop(loop_name)
-    except KeyError:
-        series_experiment_loop = None
-    return series_experiment_loop
-
-
-def _exit_if_spectra_are_missing(spectra_by_times_and_indices, series_name):
-    for (
-        _,
-        _,
-        spectrum_frame_name,
-    ), spectrum_frame in spectra_by_times_and_indices.items():
-        if not spectrum_frame:
-            msg = f'no spectrum frame found for series {series_name} for spectrum {spectrum_frame_name}'
-            exit_error(msg)
